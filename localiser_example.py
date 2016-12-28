c5223a932c18ba404adfea3da71880d8555588fa
Patrick Szmucer <patrickszmucer@gmail.com>
2016-12-28 13:01:13 +0000

    Renamed some files, added some functionality to localiser_example.py

--- localiser_example.py
+++ localiser_example.py
@@ -1,11 +1,29 @@
 from image_reader import ImageReader
 from localiser import Localiser
+from utils import *
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', action='store', required=True)
+parser.add_argument('--dataset', action='store', required=True)
+args = parser.parse_args()
+
 
 input_size = 224
-test_reader = ImageReader('datasets/monkey/test2.txt', 1, [input_size, input_size], False, False)
+test_reader = ImageReader(args.dataset, 1, [input_size, input_size], False, False)
 
-with Localiser(input_size, 'models/model14.ckpt') as localiser:
+with Localiser(input_size, args.model) as localiser:
 	for i in xrange(test_reader.total_images()):
 		images_feed, labels_feed = test_reader.next_batch()
+		gt = {'x': labels_feed[0][0:3], 'q': labels_feed[0][3:7]}
+
+		# Make prediction
 		predicted = localiser.localise(images_feed)
-		print predicted
+
+		pos_error = l2_distance(gt['x'], predicted['x'])
+		orient_error = quaternion_distance(gt['q'], predicted['q'])
+
+		print('-------------{}-------------'.format(i))
+		print('Positional error:  {}'.format(pos_error))
+		print('Orientation error: {}'.format(orient_error))
---
+++ utils.py
@@ -0,0 +1,19 @@
+import numpy as np
+import math
+
+def l2_distance(x, y):
+	if type(x) is not np.ndarray:
+		x = np.asarray(x)
+	if type(y) is not np.ndarray:
+		y = np.asarray(y)
+	return np.linalg.norm(x-y)
+
+def quaternion_distance(q1, q2):
+	"""Returns an angle that rotates q1 into q2"""
+	if type(q1) is not np.ndarray:
+		q1 = np.asarray(q1)
+	if type(q2) is not np.ndarray:
+		q2 = np.asarray(q2)
+	cos = 2*(q1.dot(q2))**2 - 1
+	cos = max(min(cos, 1), -1) # To combat numerical imprecisions
+	return math.acos(cos)
