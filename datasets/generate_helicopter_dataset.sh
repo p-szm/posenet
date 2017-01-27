42d5bd531182d24a119d8f802a1bb69d444a2438
Patrick Szmucer <patrickszmucer@gmail.com>
2017-01-27 13:15:53 +0000

    New dataset and more options to from_different_sides.py

---
+++ datasets/generate_helicopter_dataset.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+shopt -s expand_aliases
+set -e
+
+# helicopter.blend taken from https://www.blender.org/download/demo-files/
+
+BLENDER_FILENAME='../scenes/helicopter.blend'
+PYTHON_FILENAME='../from_different_sides.py'
+DIRNAME='helicopter'
+
+alias blender=/Applications/blender.app/Contents/MacOS/blender
+
+# Training dataset
+blender $BLENDER_FILENAME -b -P $PYTHON_FILENAME -- \
+	--output_dir $DIRNAME --dataset_name training --n_images 400 \
+	--r 1 2 --vary_origin 0.1 --width 256 --height 256 \
+	--spherical -3.14159 3.14159 0 3.14159 --factor 2
