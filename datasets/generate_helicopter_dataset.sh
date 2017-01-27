b01ba7bc0e5c6dd6a7e634eb7373eb8dc5fbaf31
Patrick Szmucer <patrickszmucer@gmail.com>
2017-01-27 15:39:31 +0000

    Updated the helicopter dataset

--- datasets/generate_helicopter_dataset.sh
+++ datasets/generate_helicopter_dataset.sh
@@ -13,5 +13,13 @@ alias blender=/Applications/blender.app/Contents/MacOS/blender
 # Training dataset
 blender $BLENDER_FILENAME -b -P $PYTHON_FILENAME -- \
 	--output_dir $DIRNAME --dataset_name training --n_images 400 \
-	--r 1 2 --vary_origin 0.1 --width 256 --height 256 \
+	--r 1 2 --vary_origin 0.1 --origin 0 0.3 0.4 \
+	--width 256 --height 256 \
 	--spherical -3.14159 3.14159 0 3.14159 --factor 2
+
+# Validation dataset
+blender $BLENDER_FILENAME -b -P $PYTHON_FILENAME -- \
+	--output_dir $DIRNAME --dataset_name training --n_images 100 \
+	--r 1 2 --vary_origin 0.1 --origin 0 0.3 0.4 \
+	--width 256 --height 256 \
+	--spherical -3.14159 3.14159 0 3.14159
