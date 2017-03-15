e757a1aac6a843f4303150ce354f183ccd0e376d
Patrick Szmucer <patrickszmucer@gmail.com>
2017-03-15 15:51:23 +0000

    Updated init file

--- posenet/utils/__init__.py
+++ posenet/utils/__init__.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append(os.path.dirname(__file__))
 
 from coordinates import *
+from cylinder import *
 from poisson_sampler import *
 from quaternions import *
 from utils import *
