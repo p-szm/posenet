5605aa12dc476a30ee4acd2d4c9c4711066df4ff
Patrick Szmucer <patrickszmucer@gmail.com>
2017-05-06 13:25:10 +0100

    New option to blend.py

--- blend.py
+++ blend.py
@@ -10,6 +10,7 @@ parser.add_argument('dir1')
 parser.add_argument('dir2')
 parser.add_argument('output_dir')
 parser.add_argument('-m', '--merge', action='store_true')
+parser.add_argument('-o', '--overlay', action='store_true')
 args = parser.parse_args()
 
 files1 = glob.glob('{}/*.png'.format(args.dir1))
@@ -26,7 +27,9 @@ for i, f in enumerate(files1):
     out = os.path.join(args.output_dir, os.path.basename(files1[i]))
     if args.merge:
         os.system('convert +append {} {} {}'.format(files1[i], files2[i], out))
+    if args.overlay:
+        os.system('convert {} {} -gravity center -composite {}'.format(files1[i], files2[i], out))
     else:
-        os.system('convert {} {} -evaluate-sequence mean {}'.format(files1[i], files2[i], out))
+        os.system('convert {} {} -alpha on -evaluate-sequence mean {}'.format(files1[i], files2[i], out))
 
 # To colorise: convert image.png +level-colors "red", red.png
